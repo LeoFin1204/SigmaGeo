560617279a4a93536ca6cef7fdd019b7e2d48d06
Leonid <fiorochanal@gmail.com>
2024-06-19 13:36:37 +0300

    key

--- sigmageo_root/sigmageo_site/settings.py
+++ sigmageo_root/sigmageo_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(96dt3qc5ipu1+&rak)144f&vo&@5+zsg@ay+c6@2u2yqq3&2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
